843c68e5a882c0fc59a15605349f6beb00061ad5
Ortello7 <domenicoortello00@gmail.com>
2025-01-08 23:08:49 +0100

    feat script

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Elementi del DOM
+const emailListElm = document.getElementById('email-list')
+const emailBtnElm = document.getElementById('email-btn')
+
+//Funzione per generare le email
+function generateEmail() {
+    emailListElm.innerHTML = '';
+    for (let i = 0; i < 10; i++) {
+        axios.get('https:flynn.boolean.careers/exercises/api/random/mail')
+        .then(function (response) {
+            emailListElm.innerHTML += `<li>${response.data.response}</li>`;
+        })
+    }
+}
+
+generateEmail();
+
+//Generazione delle email quando si clicca sul bottone
+emailBtnElm.addEventListener('click', generateEmail);
