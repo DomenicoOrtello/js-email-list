ef841f0ec866f65c3054061de82f77a5298ae271
Ortello7 <domenicoortello00@gmail.com>
2025-01-09 09:38:57 +0100

    fixed lkink in script

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const emailBtnElm = document.getElementById('email-btn')
 function generateEmail() {
     emailListElm.innerHTML = '';
     for (let i = 0; i < 10; i++) {
-        axios.get('https:flynn.boolean.careers/exercises/api/random/mail')
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(function (response) {
             emailListElm.innerHTML += `<li>${response.data.response}</li>`;
         })
